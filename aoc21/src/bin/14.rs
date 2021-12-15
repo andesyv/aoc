8744fad95ec53fad82f20198b6aa0d4ff62ef31a
Anders Syvertsen <andesyv@gmail.com>
2021-12-15 18:31:10 +0100

    Attempt at divide and conquering the algorithm

--- aoc21/src/bin/14.rs
+++ aoc21/src/bin/14.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 
 fn main() {
     const INPUT: &str = include_str!("../inputs/14.txt");
-    let mut polymer: Vec<char> = INPUT.lines().next().unwrap().chars().collect();
+    let polymer: Vec<char> = INPUT.lines().next().unwrap().chars().collect();
 
     let rules: HashMap<(char, char), char> = INPUT
         .lines()
@@ -17,15 +17,15 @@ fn main() {
         .collect();
 
     let now = Instant::now();
-    for _ in 0..40 {
-        polymer = polymerize(&polymer, &rules);
+    let mut frequency: HashMap<char, u64> = HashMap::new();
+    let mut it = polymer.windows(2);
+    while let Some(&[a,b]) = it.next() {
+        polymerize_count(a,b, 25, &rules, &mut frequency);
+    }
+    if let Some(v) = frequency.get_mut(polymer.last().unwrap()) {
+        *v += 1;
     }
     println!("Polymerization took {} seconds to execute", now.elapsed().as_secs());
-
-    let mut frequency: HashMap<char, u64> = HashMap::new();
-    for c in polymer {
-        *frequency.entry(c).or_default() += 1;
-    }
 
     if let (Some(max), Some(min)) = (frequency.iter().map(|t|t.1).max(), frequency.iter().map(|t|t.1).min()) {
         println!("Most common - least common = {}", max - min);
@@ -98,5 +98,18 @@ fn polymerize_n(polymer: &Vec<char>, rules: &HashMap<(char, char), char>, n: u32
         return polymer.into_iter().collect::<String>();
     } else {
         polymerize_n(&polymerize(&polymer, &rules), &rules, n-1)
+    }
+}
+
+fn polymerize_count(a: char, c: char, depth: u32, rules: &HashMap<(char, char), char>, count: &mut HashMap<char, u64>) {
+    if depth == 0 {
+        *count.entry(a).or_default() += 1;
+    } else {
+        if let Some(b) = rules.get(&(a, c)) {
+            polymerize_count(a, *b, depth - 1, &rules, count);
+            polymerize_count(*b, c, depth - 1, &rules, count);
+        } else {
+            *count.entry(a).or_default() += 1;
+        }
     }
 }
