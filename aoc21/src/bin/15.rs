3dd09a7173379aa88154d6d4236f1b3dccc30324
Anders Syvertsen <andesyv@gmail.com>
2021-12-19 00:44:02 +0100

    Completed day 15

--- aoc21/src/bin/15.rs
+++ aoc21/src/bin/15.rs
@@ -1,25 +1,44 @@
-use std::cell::RefCell;
 use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap};
-use std::rc::{Rc, Weak};
+
+type Grid = Vec<Vec<u8>>;
+
+fn main() {
+    const INPUT: &str = include_str!("../inputs/15.txt");
+    // const INPUT: &str = "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581";
+    let grid: Grid = INPUT
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap().try_into().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let (x_max, y_max) = find_grid_max(&grid);
+    let path = djikstra(&grid, (0, 0), (x_max - 1, y_max - 1));
+    // println!("Path is: {:?}", path.iter().map(|n|n.pos).collect::<Vec<(usize, usize)>>());
+    println!("Path's risk sum is {}", path.iter().map(|n|n.cost).sum::<u32>());
+
+    // Part 2:
+    let big_grid = gen_large_grid(&grid);
+    // println!("Big grid: \n{}", big_grid.iter().map(|l|l.iter().map(|n|n.to_string()).collect::<String>()).collect::<Vec<String>>().join("\n"));
+    let (x_max, y_max) = find_grid_max(&big_grid);
+    let path = djikstra(&big_grid, (0, 0), (x_max - 1, y_max - 1));
+    println!("Big grid's path's risk sum is {}", path.iter().map(|n|n.cost).sum::<u32>());
+
+}
+
+fn find_grid_max(grid: &Grid) -> (usize, usize) {
+    (grid.len(), grid.iter().nth(0).unwrap().len())
+}
 
 #[derive(Eq, Clone, Copy, Debug)]
 struct Node {
+    cost: u32,
     distance: u32,
-    cost: u8,
-    coord: (usize, usize),
-    prev: Option<(usize, usize)>,
-}
-
-impl Node {
-    fn new(cost: u8) -> Node {
-        Node {
-            distance: u32::MAX,
-            cost: cost,
-            coord: (0, 0),
-            prev: None,
-        }
-    }
+    pos: (usize, usize),
+    prev: (usize, usize),
 }
 
 impl PartialEq for Node {
@@ -40,130 +59,147 @@ impl Ord for Node {
     }
 }
 
-fn main() {
-    const INPUT: &str = "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581";
+fn djikstra(grid: &Grid, start: (usize, usize), end: (usize, usize)) -> Vec<Node> {
+    let mut checked = HashMap::new();
+    let mut search_heap = BinaryHeap::new();
 
-    let grid: Vec<Vec<Node>> = INPUT
+    // Initial nodes:
+    checked.insert(
+        start,
+        Node {
+            cost: 0,
+            distance: 0,
+            pos: start,
+            prev: start,
+        },
+    );
+    for neighbour in neighbour_indices(start) {
+        if let Some(coord) = neighbour {
+            if let Some(value) = index(grid, coord) {
+                let node = Node {
+                    cost: u32::from(*value),
+                    distance: u32::from(*value),
+                    pos: coord,
+                    prev: start,
+                };
+                if checked.insert(coord, node).is_none() {
+                    search_heap.push(node);
+                }
+            }
+        }
+    }
+
+    // Main logic:
+    while let Some(mut next) = search_heap.pop() {
+        if next.pos == end {
+            let mut rets = Vec::from([next]);
+            let mut prev = next.prev;
+            while prev != start {
+                next = *checked.get(&prev).unwrap();
+                prev = next.prev;
+                rets.push(next);
+            }
+
+            return rets.into_iter().rev().collect();
+        }
+
+        for neighbour in neighbour_indices(next.pos) {
+            if let Some(coord) = neighbour {
+                if let Some(value) = index(grid, coord) {
+                    let node = Node {
+                        cost: u32::from(*value),
+                        distance: next.distance + u32::from(*value),
+                        pos: coord,
+                        prev: next.pos,
+                    };
+                    // Cannot use insert to check, because it also updates the entry
+                    // (can use try_insert whenever it becomes stable)
+                    if !checked.contains_key(&coord) {
+                        checked.insert(coord, node);
+                        search_heap.push(node);
+                    }
+                }
+            }
+        }
+    }
+
+    vec![]
+}
+
+fn neighbour_indices((i, j): (usize, usize)) -> [Option<(usize, usize)>; 4] {
+    [
+        safe_add((i, j), (-1, 0)),
+        safe_add((i, j), (1, 0)),
+        safe_add((i, j), (0, -1)),
+        safe_add((i, j), (0, 1)),
+    ]
+}
+
+fn safe_add(a: (usize, usize), b: (i32, i32)) -> Option<(usize, usize)> {
+    let l = usize::try_from(i32::try_from(a.0).ok()? + b.0).ok()?;
+    let r = usize::try_from(i32::try_from(a.1).ok()? + b.1).ok()?;
+    Some((l, r))
+}
+
+fn index(grid: &Grid, (i, j): (usize, usize)) -> Option<&u8> {
+    grid.get(j)?.get(i)
+}
+
+fn gen_large_grid(small_grid: &Grid) -> Grid {
+    let small_grid_size = find_grid_max(small_grid);
+    let mut grid = Vec::with_capacity(small_grid_size.1 * 5);
+    for y in 0..5 {
+        for l in small_grid {
+            let mut newline = Vec::with_capacity(small_grid_size.0 * 5);
+            for x in 0..5 {
+                newline.extend(l.iter().map(|v|{
+                    let b = v + x + y;
+                    if 9 < b {
+                        b - 9
+                    } else {
+                        b
+                    }
+                }));
+            }
+            grid.push(newline);
+        }
+    }
+    grid
+}
+
+#[test]
+fn test1() {
+    const INPUT: &str = "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581";
+    let grid: Grid = INPUT
         .lines()
         .map(|l| {
             l.chars()
-                .map(|c| Node::new(c.to_digit(10).unwrap().try_into().unwrap()))
+                .map(|c| c.to_digit(10).unwrap().try_into().unwrap())
                 .collect()
         })
         .collect();
 
-    println!("Grid is: {:?}", grid);
+    let (x_max, y_max) = find_grid_max(&grid);
+    let path = djikstra(&grid, (0, 0), (x_max - 1, y_max - 1));
 
-    let grid_size: (usize, usize) = (grid.get(0).unwrap().len(), grid.len());
-
-    let shortest_path = djikstra(&grid, (0, 0), (grid_size.0 - 1, grid_size.0 - 1));
-    println!("Shortest path: {:?}", shortest_path);
+    assert_eq!(path.iter().map(|n|n.cost).sum::<u32>(), 40);
 }
 
-fn djikstra(grid: &Vec<Vec<Node>>,
-    start: (usize, usize),
-    end: (usize, usize),
-) -> Vec<(usize, usize)> {
-    let mut checked = HashMap::from([(start, Node { distance: 0, cost: 0, coord: start, prev: None })]);
-    let mut unvisited = BinaryHeap::new();
-    // let mut checked = HashSet::new();
-    let grid_indices: Vec<(usize, usize)> = grid
-        .iter()
-        .enumerate()
-        .map(|(j, v)| {
-            v.iter()
-                .enumerate()
-                .map(|(i, _)| (i, j))
-                .collect::<Vec<_>>()
+#[test]
+fn test2() {
+    const INPUT: &str = "1163751742\n1381373672\n2136511328\n3694931569\n7463417111\n1319128137\n1359912421\n3125421639\n1293138521\n2311944581";
+    let mut grid: Grid = INPUT
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap().try_into().unwrap())
+                .collect()
         })
-        .flatten()
         .collect();
 
-    let mut check_neighbours = |current_coord: (usize, usize), current_distance: u32| {
-        let surrounding = get_surrounding(current_coord);
-        let mut neighbours = Vec::new();
-        for coord in surrounding {
-            if let Some(coord) = coord {
-                let cost = index(&grid, coord).cost;
-                let node = Node {
-                    distance: current_distance + u32::from(cost),
-                    cost: cost,
-                    coord: coord,
-                    prev: Some(current_coord),
-                };
-                if checked.insert(coord, node).is_none() {
-                    neighbours.push(node)
-                }
-            }
-        }
-        neighbours
-    };
+    grid = gen_large_grid(&grid);
+    let (x_max, y_max) = find_grid_max(&grid);
+    let path = djikstra(&grid, (0, 0), (x_max - 1, y_max - 1));
 
-    let find_path = |end: &Node| {
-        let mut path = Vec::new();
-        let mut curr = Some(end.coord);
-        while let Some(coord) = curr {
-            path.push(coord);
-            if let Some(prev_node) = checked.get(&coord) {
-                curr = prev_node.prev
-            } else {
-                break;
-            }
-        }
-        path.into_iter().rev().collect()
-    };
-
-    let current_distance = u32::from(index(&grid, start).cost);
-    for node in check_neighbours(start, current_distance) {
-        unvisited.push(node);
-    }
-
-    while let Some(current) = unvisited.pop() {
-        if current.coord == end {
-            return find_path(index(&grid, current.coord));
-        }
-        unvisited.extend(check_neighbours(current.coord, current.distance).into_iter());
-    }
-    vec![]
-}
-
-fn index_mut(grid: &mut Vec<Vec<Node>>, (i, j): (usize, usize)) -> &mut Node {
-    grid.get_mut(j).unwrap().get_mut(i).unwrap()
-}
-
-fn index(grid: &Vec<Vec<Node>>, (i, j): (usize, usize)) -> &Node {
-    grid.get(j).unwrap().get(i).unwrap()
-}
-
-fn get_coords(grid: &Vec<Vec<Node>>, node: &Node) -> Option<(usize, usize)> {
-    for j in 0..grid.len() {
-        for i in 0..grid.get(j).unwrap().len() {
-            return Some((i, j));
-        }
-    }
-    None
-}
-
-fn get_surrounding((i, j): (usize, usize)) -> [Option<(usize, usize)>; 8] {
-    let mut n = 0;
-    let mut out = [None; 8];
-    for y in -1..2 {
-        for x in -1..2 {
-            if y != 0 || x != 0 {
-                if let (Some(a), Some(b)) = (checked_add_signed(i, x), checked_add_signed(j, y)) {
-                    out[n] = Some((a, b));
-                }
-                n += 1;
-            }
-        }
-    }
-    out
-}
-
-fn checked_add_signed(a: usize, b: isize) -> Option<usize> {
-    if b < 0 {
-        a.checked_sub(b.checked_neg()?.try_into().ok()?)
-    } else {
-        a.checked_add(b.try_into().ok()?)
-    }
+    assert_eq!(path.iter().map(|n|n.cost).sum::<u32>(), 315);
 }
