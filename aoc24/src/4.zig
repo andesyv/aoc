ce7734f7d8310b691c237f662090920213133f78
Andough <andesyv@gmail.com>
2024-12-04 22:03:44 +0100

    Day 4 Part 2

--- aoc24/src/4.zig
+++ aoc24/src/4.zig
@@ -38,7 +38,7 @@ fn wrappedAdd(a: u32, b: i8) u32 {
     return if (b < 0) a -% @as(u32, @intCast(-b)) else a + @as(u32, @intCast(b));
 }
 
-fn findXMAS(grid: Grid, search_slice: []const u8, pos: [2]u32, dir: [2]i8) bool {
+fn findPattern(grid: Grid, search_slice: []const u8, pos: [2]u32, dir: [2]i8) bool {
     // Base case: We've found the entire string
     if (search_slice.len == 0) {
         return true;
@@ -56,7 +56,7 @@ fn findXMAS(grid: Grid, search_slice: []const u8, pos: [2]u32, dir: [2]i8) bool 
     const x = wrappedAdd(pos[0], dir[0]);
     const y = wrappedAdd(pos[1], dir[1]);
     const new_pos = [_]u32{ x, y };
-    return findXMAS(grid, search_slice[1..], new_pos, dir);
+    return findPattern(grid, search_slice[1..], new_pos, dir);
 }
 
 fn findCountOfAllXMAS(grid: Grid) u32 {
@@ -79,10 +79,87 @@ fn findCountOfAllXMAS(grid: Grid) u32 {
         for (0..grid.height) |y| {
             for (dirs) |dir| {
                 const pos = [_]u32{ @as(u32, @intCast(x)), @as(u32, @intCast(y)) };
-                if (findXMAS(grid, search_slice, pos, dir)) {
+                if (findPattern(grid, search_slice, pos, dir)) {
                     // std.debug.print("Found an XMAS at pos {any}, dir: {any}\n", .{ pos, dir });
                     count += 1;
                 }
+            }
+        }
+    }
+
+    return count;
+}
+
+fn posMatchesCrossPattern(grid: Grid, x: u32, y: u32) bool {
+    // Early test
+    if (grid.getCharAtPos(x, y)) |char| {
+        if (char != 'A') {
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    // So I misread the task and though we were supposed to find X's in cardinal directions as well and made
+    // this extra logic for it...
+    // const cardinals = [_][2]i8{
+    //     .{  1,  0 },
+    //     .{  0,  1 },
+    //     .{ -1,  0 },
+    //     .{  0, -1 },
+    // };
+
+    const diagonals = [_][2]i8{
+        .{  1,  1 },
+        .{ -1,  1 },
+        .{ -1, -1 },
+        .{  1, -1 },
+    };
+
+    const search_slice = "MAS";
+
+    var mas_count: u32 = 0;
+    // for (cardinals) |dir| {
+    //     // Move the pos 1 "back" from it's direction. E.g. pos: (0, 0), dir: (1, 0) => new pos (-1, 0)
+    //     const pos = [_]u32{ wrappedAdd(x, -dir[0]), wrappedAdd(y, -dir[1]) };
+    //     if (findPattern(grid, search_slice, pos, dir)) {
+    //         // std.debug.print("Found an XMAS at pos {any}, dir: {any}\n", .{ pos, dir });
+    //         mas_count += 1;
+    //     }
+    // }
+    //
+    // if (mas_count == 2) {
+    //     return true;
+    // } else if (2 < mas_count) {
+    //     std.debug.panic("How?", .{});
+    // }
+    //
+    // mas_count = 0;
+    for (diagonals) |dir| {
+        // Move the pos 1 "back" from it's direction. E.g. pos: (0, 0), dir: (1, 0) => new pos (-1, 0)
+        const pos = [_]u32{ wrappedAdd(x, -dir[0]), wrappedAdd(y, -dir[1]) };
+        if (findPattern(grid, search_slice, pos, dir)) {
+            // std.debug.print("Found an XMAS at pos {any}, dir: {any}\n", .{ pos, dir });
+            mas_count += 1;
+        }
+    }
+
+    if (mas_count == 2) {
+        return true;
+    } else if (2 < mas_count) {
+        std.debug.panic("How?", .{});
+    }
+
+    return false;
+}
+
+fn findCountOfAllCrossMASPatterns(grid: Grid) u32 {
+    var count: u32 = 0;
+
+    for (0..grid.width) |x| {
+        for (0..grid.height) |y| {
+            if (posMatchesCrossPattern(grid, @as(u32, @intCast(x)), @as(u32, @intCast(y)))) {
+                count += 1;
             }
         }
     }
@@ -97,6 +174,10 @@ pub fn main() void {
     const grid = parseToGrid(input);
     const stdout = std.io.getStdOut();
     std.fmt.format(stdout.writer(), "Count of all XMAS words: {d}\n", .{findCountOfAllXMAS(grid)}) catch |err| {
+        std.debug.panic("Writing to stdout failed with the following error: {any}\n", .{err});
+    };
+
+    std.fmt.format(stdout.writer(), "Count of all X-MAS words: {d}\n", .{findCountOfAllCrossMASPatterns(grid)}) catch |err| {
         std.debug.panic("Writing to stdout failed with the following error: {any}\n", .{err});
     };
 }
@@ -149,4 +230,10 @@ test "word hunt" {
     const grid = parseToGrid(example_input);
     const result = findCountOfAllXMAS(grid);
     try std.testing.expectEqual(18, result);
+}
+
+test "cross MAS word hunt" {
+    const grid = parseToGrid(example_input);
+    const result = findCountOfAllCrossMASPatterns(grid);
+    try std.testing.expectEqual(9, result);
 }
